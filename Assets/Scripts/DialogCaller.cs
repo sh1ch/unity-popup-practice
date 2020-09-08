56518de1ebc334b2a33a82aa2c5b257cc4ddec54
sh1ch <isogucchi-code@outlook.com>
2020-09-08 13:09:29 +0900

    fixed: Close の対象を修正。

--- Assets/Scripts/DialogCaller.cs
+++ Assets/Scripts/DialogCaller.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DialogCaller : MonoBehaviour
 {
+    private GameObject _DialogPrefab;
+
+    [SerializeField]
+    private GameObject _Canvas = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _DialogPrefab = Resources.Load("PopupDialog") as GameObject;
     }
 
     // Update is called once per frame
@@ -18,6 +23,8 @@ public class DialogCaller : MonoBehaviour
 
     public void PopupTest()
     {
+        var instance = Instantiate<GameObject>(_DialogPrefab);
 
+        instance.transform.SetParent(_Canvas.transform, false);
     }
 }
---
+++ Assets/Scripts/PopupCloser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupCloser : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _PopupDialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseTest(float delaySecond = 0.0F)
+    {
+        Destroy(_PopupDialog, delaySecond);
+    }
+}
